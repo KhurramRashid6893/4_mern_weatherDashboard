0f3ac86c030c2145d899b2f4f71b8615952629ad
Khurram Rashid <98824765+KhurramRashid6893@users.noreply.github.com>
2025-07-03 19:13:00 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,7 +11,7 @@ const {
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/login", loginUser); 
 router.get("/profile", protect, getProfile);
 router.post("/favorites", protect, addFavorite);
 router.delete("/favorites/:city", protect, removeFavorite);
